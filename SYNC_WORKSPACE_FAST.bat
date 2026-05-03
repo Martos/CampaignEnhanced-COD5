71e7be4a2f866fc9cf93b8b1ad9c1acb5f49bc6f
Mauro Cipriani <mauro.cipriani95@gmail.com>
2026-05-03 21:16:04 +0200

    Add utility scripts

--- SYNC_WORKSPACE_FAST.bat
+++ SYNC_WORKSPACE_FAST.bat
@@ -1,4 +1,15 @@
 @echo off
+setlocal enabledelayedexpansion
+
+if exist .env (
+    for /f "tokens=*" %%i in (.env) do (
+        set "%%i"
+    )
+) else (
+    echo Errore: .env not found.
+    pause
+    exit /b
+)
 
 xcopy %WORKDIR_PATH%\raw\*.* "%CODWAW_PATH%\raw\" /Y /E /H /C /I
 xcopy %WORKDIR_PATH%\zone_source\*.* "%CODWAW_PATH%\zone_source\" /Y /E /H /C /I
