b8dafeabaa261ebd166c6ee11d5448957573938e
Mauro Cipriani <mauro.cipriani95@gmail.com>
2026-05-03 21:22:34 +0200

    Add utility scripts

--- RUN_GAME.bat
+++ RUN_GAME.bat
@@ -1,9 +1,20 @@
 @echo off
+setlocal enabledelayedexpansion
 
-E:
+if exist .env (
+    for /f "tokens=*" %%i in (.env) do (
+        set "%%i"
+    )
+) else (
+    echo Errore: .env not found.
+    pause
+    exit /b
+)
+
+%LINKER_DISK_PATH%
 
 cd "%CODWAW_PATH%\"
 
-START "" CoDWaW_LanFixed.exe +set fs_game "mods/CampaignEnhanced" +set r_fullscreen 0 +set r_mode "1024x768" +set developer 1 +set ui_showList 0 +exec "default_controller.cfg"
+START "" CoDWaW_LanFixed.exe +set fs_game "mods/CampaignEnhanced" +set r_fullscreen 0 +set r_mode "1280x720" +set developer 2 +set ui_showList 1 +exec "default_controller.cfg"
 
 exit /b
