e2e36a1497eefa3a150e753ec333e6b7897431c6
Mauro Cipriani <mauro.cipriani95@gmail.com>
2026-05-04 20:28:02 +0200

    Add utility scripts

--- SYNC_WORKSPACE_FAST.bat
+++ SYNC_WORKSPACE_FAST.bat
@@ -16,4 +16,7 @@ xcopy %WORKDIR_PATH%\zone_source\*.* "%CODWAW_PATH%\zone_source\" /Y /E /H /C /I
 xcopy %WORKDIR_PATH%\mods\CampaignEnhanced\weapons\*.* "%CODWAW_PATH%\mods\CampaignEnhanced\weapons\" /Y /E /H /C /I
 xcopy %WORKDIR_PATH%\mods\CampaignEnhanced\sound\*.* "%CODWAW_PATH%\mods\CampaignEnhanced\sound\" /Y /E /H /C /I
 
+copy /Y "%WORKDIR_PATH%\mods\CampaignEnhanced\mod.csv" "%CODWAW_PATH%\mods\CampaignEnhanced"
+copy /Y "%WORKDIR_PATH%\mods\CampaignEnhanced\mod.arena" "%CODWAW_PATH%\mods\CampaignEnhanced"
+
 exit /b
